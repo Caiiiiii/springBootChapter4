c9ed90803b73516150ab4c303e7f7f6ff0f2fe8d
Caiiiiiii <995017591@qq.com>
2018-08-23 15:55:06 +0800

    Initial commit

---
+++ src/main/java/WebInitializer.java
@@ -0,0 +1,22 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(MyMvcConfig.class);
+        ctx.setServletContext(servletContext);
+        ServletRegistration.Dynamic context = servletContext.addServlet(
+                "dispatcher", new DispatcherServlet(ctx));
+
+        context.addMapping("/");
+        context.setLoadOnStartup(1);
+    }
+}
